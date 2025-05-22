6f127ca037da0dffd91a6b85c944b1bfb150784a
Bhaargav <144582477+bhaargav04@users.noreply.github.com>
2025-05-22 15:33:14 +0530

    Little Contribution

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.js";
+import authRouter from './routes/auth.js';
 import userRoutes from "./routes/users.js";
 import bookRoutes from "./routes/books.js";
 import transactionRoutes from "./routes/transactions.js";
@@ -11,13 +12,20 @@ import categoryRoutes from "./routes/categories.js";
 /* App Config */
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT;
+
 
 /* Middlewares */
 app.use(express.json());
 app.use(cors());
 
+app.use(cors({
+  origin: 'http://localhost:5173', // Allow requests from frontend
+  credentials: true,
+}));
+
 /* API Routes */
+app.use('/api', authRouter);
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/books", bookRoutes);
@@ -25,8 +33,7 @@ app.use("/api/transactions", transactionRoutes);
 app.use("/api/categories", categoryRoutes);
 
 /* MongoDB connection */
-mongoose.connect(
-  process.env.MONGO_URL,
+mongoose.connect(process.env.MONGO_URL,
   {
     useCreateIndex: true,
     useNewUrlParser: true,
